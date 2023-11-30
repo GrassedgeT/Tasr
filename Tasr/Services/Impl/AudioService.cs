0dba9abecba45222e30316260ec59edfefad4a52
qianniancao <grassedge@outlook.com>
2023-11-30 23:04:09 +0800

    正在进行

---
+++ Tasr/Services/Impl/AudioService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasr.Library.Services;
+
+namespace Tasr.Services.Impl
+{
+    public class AudioService : IAudioService
+    {
+        private readonly IAlertService _alertService;
+
+        public AudioService(IAlertService alertService)
+        {
+            _alertService = alertService;
+        }
+        public Task<byte[]> PickAudioAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> RecordAudioAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

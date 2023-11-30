f59c09b29508b5d21feccb306d6966f0a4d43da3
qianniancao <grassedge@outlook.com>
2023-11-30 22:26:14 +0800

    导航业务逻辑初步完成

---
+++ Tasr.Library/Services/INavigationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasr.Library.Services
+{
+    public interface INavigationService
+    {
+        void NavigateTo(string uri);
+        void NavigateTo(string uri, object parameter);
+    }
+
+    public static class NavigationServiceConstants
+    {
+        public const string NewRec = "/newrec";
+        public const string History = "/history";
+    }
+}
---
+++ Tasr.Library/Services/IParcelBoxService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasr.Library.Services
+{
+    public interface IParcelBoxService
+    {
+        string Put(object o);
+        object Get(string key);
+    }
+}

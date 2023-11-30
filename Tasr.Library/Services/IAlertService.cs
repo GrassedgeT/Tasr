de4baabd86b82e1c7989a025a8a596eb27cf5c09
qianniancao <grassedge@outlook.com>
2023-11-30 22:25:51 +0800

    通知业务逻辑完成

---
+++ Tasr.Library/Services/IAlertService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasr.Library.Services
+{
+    public interface IAlertService
+    {
+        Task AlertAsync(string title, string message, string button);
+    }
+}
---
+++ Tasr.Library/Services/Impl/AlertService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BootstrapBlazor.Components;
+
+namespace Tasr.Library.Services.Impl
+{
+    public class AlertService : IAlertService
+    {
+        private readonly SwalService _swalService;
+
+        public AlertService(SwalService swalService)
+        {
+            _swalService = swalService;
+        }
+
+        public async Task AlertAsync(string title, string message, string button) =>
+            await _swalService.Show(new SwalOption
+            {
+                Category = SwalCategory.Error,
+                Title = title,
+                Content = message,
+                CloseButtonText = button
+            });
+
+    }
+}

b87b3e1e952709d80fd0dec38ba7ad4be544f082
qianniancao <grassedge@outlook.com>
2023-12-05 10:34:44 +0800

    选取文件完成

--- Tasr.Library/Pages/NewRec.razor.cs
+++ Tasr.Library/Pages/NewRec.razor.cs
@@ -8,5 +8,20 @@ namespace Tasr.Library.Pages
 {
     public partial class NewRec
     {
+        private async Task PickAsync()
+        {
+            var bytes = await _audioService.PickAudioAsync();
+
+            if (bytes is null)
+            {
+                return;
+            }
+            await _AudioToTextService.AudioToTextAsync(bytes);
+        }
+
+        private async Task RecordAsync()
+        {
+            await _audioService.RecordAudioAsync();
+        }
     }
 }

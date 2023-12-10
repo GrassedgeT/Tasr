ed5f6c24498863145b818fa228bdc90470e7c3cb
qianniancao <grassedge@outlook.com>
2023-12-10 20:13:01 +0800

    上传音频识别完成

--- Tasr/Services/Impl/AudioService.cs
+++ Tasr/Services/Impl/AudioService.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tasr.Library.Services;
+using Tasr.Library.Utils;
+using Tasr.Models;
+
 
 namespace Tasr.Services.Impl
 {
@@ -11,18 +14,45 @@ namespace Tasr.Services.Impl
     {
         private readonly IAlertService _alertService;
 
+        private readonly PickOptions wavOptions = new()
+        {
+            PickerTitle = "请选择wav格式的音频文件",
+            FileTypes = PickFileTypes.wavFileType
+        };  
+
         public AudioService(IAlertService alertService)
         {
             _alertService = alertService;
         }
-        public Task<byte[]> PickAudioAsync()
+        public async Task<byte[]> PickAudioAsync()
         {
-            throw new NotImplementedException();
+            var audio = await FilePicker.Default.PickAsync(wavOptions);
+
+            if (audio is null)
+            {
+                await _alertService.AlertAsync(
+                    ErrorMessages.AudioErrorTitle, 
+                    "音频选择失败", 
+                    ErrorMessages.ErrorButton
+                );
+                return null;
+            }
+
+            await using var audioStream = await audio.OpenReadAsync();
+            using var ms = new MemoryStream();
+            await audioStream.CopyToAsync(ms);
+            return ms.ToArray(); 
         }
 
-        public Task<byte[]> RecordAudioAsync()
+        public async Task<byte[]> RecordAudioAsync()
         {
-            throw new NotImplementedException();
+            await _alertService.AlertAsync(
+                ErrorMessages.AudioErrorTitle, 
+                "音频选择失败", 
+                ErrorMessages.ErrorButton
+            );
+            return null;
+
         }
     }
 }

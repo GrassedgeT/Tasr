cf97800d51f3d59507b8e21c54a7d2d5e0b29e19
qianniancao <grassedge@outlook.com>
2023-12-10 20:14:49 +0800

    初步完成

--- Tasr.Library/Utils/ErrorMessages.cs
+++ Tasr.Library/Utils/ErrorMessages.cs
@@ -9,7 +9,9 @@ namespace Tasr.Library.Utils
     public partial class ErrorMessages
     {
         public const string AudioErrorTitle = "音频错误";
-        public const string AudioErrorButton = "确定";
-
+        public const string ErrorButton = "确定";
+        public const string HttpClientErrorTitle = "网络连接错误";
+        public static string GetHttpClientError(string server, string message) =>
+            $"与{server}连接时发生了错误：\n{message}";
     }
 }

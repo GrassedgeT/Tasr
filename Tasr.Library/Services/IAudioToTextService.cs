5656f95b37b813f78cc254c322bd3d4937a4a92c
qianniancao <grassedge@outlook.com>
2023-12-10 20:14:18 +0800

    初步完成

---
+++ Tasr.Library/Services/IAudioToTextService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasr.Library.Services
+{
+    public interface IAudioToTextService
+    {
+        Task<string> FileToTextAsync(byte[] audio);
+
+        //TODO 流式语音识别接口
+    }
+}
---
+++ Tasr.Library/Services/Impl/AudioToTextService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Tasr.Library.Utils;
+using TheSalLab.GeneralReturnValues;
+
+namespace Tasr.Library.Services.Impl
+{
+    public class AudioToTextService : IAudioToTextService
+    {
+        private readonly IAlertService _alertService;
+
+        private readonly string server = "Tasr服务器";
+        public AudioToTextService(IAlertService alertService)
+        {
+            _alertService = alertService;
+        }
+
+        public async Task<string> FileToTextAsync(byte[] audio)
+        {
+            using var httpClient = new HttpClient();
+
+            using var formData = new MultipartFormDataContent();
+            formData.Add(new ByteArrayContent(audio), "file", "audio.wav");
+            HttpResponseMessage response;
+            try
+            {
+                response = await httpClient.PostAsync("http://localhost:8080/home/audiotoText", formData);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception e)
+            {
+                await _alertService.AlertAsync(
+                        ErrorMessages.HttpClientErrorTitle,
+                        ErrorMessages.GetHttpClientError(server, e.Message),
+                        ErrorMessages.ErrorButton                    );
+                return null;
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<ServiceResultViewModel<string>>(
+                json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true, IncludeFields = true
+                });
+            if (result.Status != ServiceResultStatus.Succeeded)
+            {
+                await _alertService.AlertAsync(
+                   ErrorMessages.HttpClientErrorTitle,
+                   string.Join(" ", result.Messages),
+                   ErrorMessages.ErrorButton
+                );
+                return null;
+            }
+
+            return result.Result;
+        }
+    }
+}
